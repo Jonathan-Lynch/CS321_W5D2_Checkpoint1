7a9d0eec9f0c8108cce185eddb5674567eac552e
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-07-12 20:33:48 -0500

    Finished

--- CS321_W5D2_BlogAPI.Core/Services/PostService.cs
+++ CS321_W5D2_BlogAPI.Core/Services/PostService.cs
@@ -19,10 +19,16 @@ namespace CS321_W5D2_BlogAPI.Core.Services
 
         public Post Add(Post newPost)
         {
-            // TODO: Prevent users from adding to a blog that isn't theirs
-            //     Use the _userService to get the current users id.
-            //     You may have to retrieve the blog in order to check user id
-            // TODO: assign the current date to DatePublished
+            var currentUserId = _userService.CurrentUserId;
+            var currentBlog = _blogRepository.Get(newPost.BlogId);
+
+            if (currentUserId != currentBlog.UserId)
+			{
+                throw new Exception("You cannot access that blog, it aint yours. stop trying to mess up other peoples work");
+            }
+
+            newPost.DatePublished = DateTime.Now;
+
             return _postRepository.Add(newPost);
         }
 
@@ -44,13 +50,25 @@ namespace CS321_W5D2_BlogAPI.Core.Services
         public void Remove(int id)
         {
             var post = this.Get(id);
-            // TODO: prevent user from deleting from a blog that isn't theirs
+            //prevent user from deleting from a blog that isn't theirs
+            var currentUserId = _userService.CurrentUserId;
+
+            if (post.Blog.UserId != currentUserId)
+			{
+                throw new Exception("You cannot delete a blog that does not belong to you");
+            }
             _postRepository.Remove(id);
         }
 
         public Post Update(Post updatedPost)
         {
             // TODO: prevent user from updating a blog that isn't theirs
+            var currentUserId = _userService.CurrentUserId;
+            
+            if (currentUserId != updatedPost.Blog.UserId)
+			{
+                throw new Exception("You cannot update a blog that does not belong to you");
+            }
             return _postRepository.Update(updatedPost);
         }
 
--- CS321_W5D2_BlogAPI.Infrastructure/Data/PostRepository.cs
+++ CS321_W5D2_BlogAPI.Infrastructure/Data/PostRepository.cs
@@ -9,45 +9,68 @@ namespace CS321_W5D2_BlogAPI.Infrastructure.Data
 {
     public class PostRepository : IPostRepository
     {
+        private readonly AppDbContext _dbContext;
         public PostRepository(AppDbContext dbContext) 
-        {  
+        {
+            _dbContext = dbContext;
         }
 
         public Post Get(int id)
         {
-            // TODO: Implement Get(id). Include related Blog and Blog.User
-            throw new NotImplementedException();
+            //Implement Get(id). Include related Blog and Blog.User
+            return _dbContext.Posts
+                .Include(p => p.Blog)
+                .ThenInclude(p => p.User)
+                .SingleOrDefault(p => p.Id == id);
         }
 
         public IEnumerable<Post> GetBlogPosts(int blogId)
         {
-            // TODO: Implement GetBlogPosts, return all posts for given blog id
-            // TODO: Include related Blog and AppUser
-            throw new NotImplementedException();
+            //Implement GetBlogPosts, return all posts for given blog id
+            //Include related Blog and AppUser
+            return _dbContext.Posts
+                .Include(p => p.Blog)
+                .ThenInclude(p => p.User)
+                .Where(p => p.BlogId == blogId);
         }
 
         public Post Add(Post Post)
         {
-            // TODO: add Post
-            throw new NotImplementedException();
+            //add Post
+            _dbContext.Posts.Add(Post);
+            _dbContext.SaveChanges();
+            return Post;
         }
 
         public Post Update(Post Post)
         {
-            // TODO: update Post
-            throw new NotImplementedException();
+            //update Post
+            var currentPost = _dbContext.Posts.Find(Post.Id);
+            if (currentPost == null) return null;
+            _dbContext.Entry(currentPost)
+                .CurrentValues
+                .SetValues(Post);
+            _dbContext.Posts.Update(currentPost);
+            _dbContext.SaveChanges();
+            return currentPost;
         }
 
         public IEnumerable<Post> GetAll()
         {
-            // TODO: get all posts
-            throw new NotImplementedException();
+            //get all posts
+            return _dbContext.Posts
+                .Include(p => p.Blog);
         }
 
         public void Remove(int id)
         {
-            // TODO: remove Post
-            throw new NotImplementedException();
+            //remove Post
+            var currentPost = _dbContext.Posts.FirstOrDefault(p => p.Id == id);
+            if (currentPost != null)
+			{
+                _dbContext.Posts.Remove(currentPost);
+                _dbContext.SaveChanges();
+			}
         }
 
     }
